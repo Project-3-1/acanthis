16d81e6594756e41662022869510736d5b61168b
waterfl0w <jan@krueger-jan.de>
2020-11-24 15:06:48 +0100

    expand launch file

--- src/EmergencyNode.cpp
+++ src/EmergencyNode.cpp
@@ -1,14 +1,21 @@
 #include "ros/ros.h"
 #include "acanthis/Shutdown.h"
-#
+#include "crazyflie_driver/FullState.h"
+
+
+void imu_callback(const crazyflie_driver::FullState::ConstPtr& msg)
+{
+  ROS_INFO("I got a message from the IMU");
+}
+
 
 int main(int argc, char **argv)
 {
 
   ros::init(argc, argv, "EmergencyNode");
   ros::NodeHandle node;
-  //ros::Subscriber sub = n.subscribe("crazyflie/imu", 10, imu_callback);
-
+  ros::Subscriber sub = node.subscribe("crazyflie/imu", 10, imu_callback);
+  ros::spin();
   /*ros::Publisher state_publisher = n.advertise<acanthis::Shutdown>("EmergencyNode", 1);
 
   ros::Rate loop_rate(10);
@@ -32,8 +39,3 @@ int main(int argc, char **argv)
   return 0;
 }
 
-
-/*void imu_callback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
-}*/
