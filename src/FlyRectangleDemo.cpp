00354bea48eec1e7c4e27e4df5e1ad9442402037
waterfl0w <jan@krueger-jan.de>
2020-11-27 13:37:12 +0100

    /crazyflie/ranger_deck to publish measurements added

--- src/FlyRectangleDemo.cpp
+++ src/FlyRectangleDemo.cpp
@@ -1,6 +1,7 @@
 
 #include "ros/ros.h"
 #include "crazyflie_driver/Takeoff.h"
+#include "crazyflie_driver/UpdateParams.h"
 #include "crazyflie_driver/Hover.h"
 #include "crazyflie_driver/Position.h"
 #include "std_msgs/Empty.h"
@@ -12,17 +13,12 @@ int main(int argc, char **argv)
 
   ros::init(argc, argv, "FlyRectangleDemo", ros::init_options::AnonymousName);
   ros::NodeHandle n("~");
-  ros::Rate rate(2);
+  ros::Rate rate(10);
 
-//  ROS_INFO("Waiting for /crazyflie/takeoff");
-//  ros::ServiceClient takeoffService = n.serviceClient<crazyflie_driver::Takeoff>("/crazyflie/takeoff");
-//  takeoffService.waitForExistence();
-//  ROS_INFO("/crazyflie/takeoff alive");
-
-//  crazyflie_driver::Takeoff takeoff;
-//  takeoff.request.duration = ros::Duration(5);
-//  takeoff.request.groupMask = 1;
-//  takeoff.request.height = 0.5;
+  ROS_INFO("Waiting for /crazyflie/takeoff");
+  ros::ServiceClient takeoffService = n.serviceClient<crazyflie_driver::Takeoff>("/crazyflie/takeoff");
+  takeoffService.waitForExistence();
+  ROS_INFO("/crazyflie/takeoff alive");
 
   ros::Publisher cmd_position_pub = n.advertise<crazyflie_driver::Position>("/crazyflie/cmd_position", 1);
 
@@ -30,24 +26,31 @@ int main(int argc, char **argv)
   position.header.seq = 0;
   position.header.stamp = ros::Time::now();
 
-  while (ros::ok()) {
-    for(int i = 0; i < 3; i++) {
-      ROS_INFO("a");
+  while(ros::ok()) {
+    for(int i = 0; i < 10; i++) {
       position.header.seq++;
       position.header.stamp = ros::Time::now();
       position.x = 0;
       position.y = 0;
-      position.z += 0.1;
+      position.z = i / 25.0;
+      position.yaw = 0;
       cmd_position_pub.publish(position);
-      ros::Duration(0.1).sleep();
-      ROS_INFO("b");
+      rate.sleep();
+    }
+    for(int i = 0; i < 20; i++) {
+      position.header.seq++;
+      position.header.stamp = ros::Time::now();
+      position.x = 0;
+      position.y = 0;
+      position.z = 0.4;
+      position.yaw = 0;
+      cmd_position_pub.publish(position);
+      rate.sleep();
     }
     break;
   }
 
-
   while (ros::ok()) {
-
       position.header.seq++;
       position.header.stamp = ros::Time::now();
       position.x = 0;
@@ -55,40 +58,7 @@ int main(int argc, char **argv)
       position.z = 0.4;
       cmd_position_pub.publish(position);
       rate.sleep();
-      break;
   }
-
-
-
-
-//  ros::Publisher chatter_pub = n.advertise<crazyflie_driver::Hover>("/crazyflie/cmd_hover", 1);
-
-//  crazyflie_driver::Hover hover;
-//  hover.header.seq = 0;
-//  hover.header.stamp = ros::Time::now();
-
-//  while (ros::ok()) {
-//    ROS_INFO("a");
-
-//    hover.vx = 0.001;
-//    hover.vy = 0.001;
-//    hover.yawrate = 0;
-//    hover.zDistance = 0.5;
-//    chatter_pub.publish(hover);
-//    hover.header.seq++;
-//    ros::spinOnce();
-//    rate.sleep();
-//  }
-  //--- call services
-  //ros::service::waitForService("takeoff", ros::Duration(2));
-  //ros::ServiceClient client = node.serviceClient<crazyflie_driver::Takeoff>("takeoff");
-  //crazyflie_driver::Takeoff request;
-  //request.request.groupMask = 0;
-  //request.request.height = 0.3;
-  //request.request.duration = ros::Duration(5, 0);
-  //bool x = client.call(request);
-  //ROS_INFO("%d", x);
-
 
   return 0;
 }
